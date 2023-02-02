b18bbcc9c53c7445ecc871b7a301c43b614123f6
Allie Howe <howeisallie@gmail.com>
2023-02-02 21:31:50 +0000

    added links to icons

--- src/Splash.jsx
+++ src/Splash.jsx
@@ -1,7 +1,12 @@
 import { Box } from '@mui/material'
 import { HeadingText } from './Navbar'
 
-const icons = ['twitter', 'email', 'github', 'linkedin'];
+const icons = [
+  {imgPath: 'twitter', link: 'https://www.twitter.com/ScaredComputers'},
+  {imgPath: 'email', link: 'mailto:howeisallie@gmail.com'},
+  {imgPath: 'github', link: 'https://www.github.com/Allie-Howe'},
+  {imgPath: 'linkedin', link: 'https://linkedin.com/in/Allie-Howe'}
+];
 
 export const Splash = () => {
   return (
@@ -9,8 +14,10 @@ export const Splash = () => {
       <HeadingText sx={{fontSize: 175, color: '#000A'}}>hey.</HeadingText>
       <Box sx={{display: 'flex', justifyContent: 'center', gap: 5, mt: 5}}>
         {
-          icons.map((icon, index) => (
-            <img key={index} alt={icon} src={`${process.env.PUBLIC_URL}/images/icons/${icon}.svg`} width='100px' />
+          icons.map(({imgPath, link}, index) => (
+            <a href={link} target='_blank' rel='noopener noreferrer'>
+              <img key={index} alt={imgPath} src={`${process.env.PUBLIC_URL}/images/icons/${imgPath}.svg`} width='100px' />
+            </a>
           ))
         }
       </Box>
