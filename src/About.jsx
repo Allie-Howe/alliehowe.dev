bfa89cb8ab8bfcc054d25f1890097ad015bc0e01
Allie Howe <howeisallie@gmail.com>
2023-02-02 23:20:13 +0000

    now using smaller webp selfie

--- src/About.jsx
+++ src/About.jsx
@@ -13,7 +13,7 @@ export const About = () => {
     <Box>
       <HeadingText sx={{fontSize: 50}} >who am i?</HeadingText>
       {/* TODO: Should be responsive */}
-      <img style={{margin: 5, float: 'right', height: 300, borderRadius: '5%'}} alt='selfie of allie' src={`${process.env.PUBLIC_URL}/images/allie.png`} />
+      <img style={{margin: 5, float: 'right', height: 300, borderRadius: '5%'}} alt='selfie of allie' src={`${process.env.PUBLIC_URL}/images/allie.webp`} />
       <Box sx={{display: 'flex', flexDirection: 'column', gap: 2}}>
         <Typography>
           I'm Allie Howe, a Computer Science BSc (Hons) graduate from the University of Lincoln, currently working for music tech startup <NewTabLink href='http://clicknclear.com/'>ClicknClear</NewTabLink> as a Graduate Software Engineer.
