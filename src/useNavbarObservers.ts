eb3a53266546af4da77bf4549b2ee585d69636b0
Allie Howe <howeisallie@gmail.com>
2023-03-04 01:11:43 +0000

    navbar observer reliability

--- src/useNavbarObservers.ts
+++ src/useNavbarObservers.ts
@@ -1,20 +1,26 @@
-import { useRef, useEffect, useState } from 'react';
+import { useRef, useEffect, useState, useMemo } from 'react';
 
 const splashOptions = { rootMargin: "-100px 0px 0px 0px" };
 
 export const useNavbarObservers = () => {
   const splashRef = useRef<HTMLDivElement>();
   const topRef = useRef<HTMLDivElement>();
-  const [position, setPosition] = useState('top');
+  const [isTop, setIsTop] = useState(true);
+  const [isSplash, setIsSplash] = useState(false);
+
+  const position = useMemo(() => {
+    if (isTop) return 'top';
+    return isSplash ? 'light' : 'dark'
+  }, [isTop, isSplash])
 
   useEffect(() => {
     new IntersectionObserver(([entry]) => {
-      setPosition(entry.isIntersecting ? 'light' : 'dark');
-    }, splashOptions).observe(splashRef.current);
+      setIsTop(entry.isIntersecting);
+    }).observe(topRef.current);
 
     new IntersectionObserver(([entry]) => {
-      setPosition(entry.isIntersecting ? 'top' : 'light');
-    }).observe(topRef.current);
+      setIsSplash(entry.isIntersecting);
+    }, splashOptions).observe(splashRef.current);
   }, [splashRef, topRef]);
 
   return {
