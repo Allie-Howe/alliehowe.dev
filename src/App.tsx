fca8a99faa9c970121d563ab92280321e3ed2029
Allie Howe <howeisallie@gmail.com>
2023-06-02 21:50:07 +0100

    total redesign - just fckn gutted everything innit

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,6 @@
-import { Box, Container, CssBaseline, ThemeProvider } from '@mui/material';
-import { About } from './About';
-import { Navbar } from './Navbar';
-import { ProjectCards } from './ProjectCards';
-import { Splash } from './Splash';
-import { darkTheme } from './Themes/darkTheme';
-import { useNavbarObservers } from './useNavbarObservers';
-
-function App() {
-  const { splashRef, topRef, position } = useNavbarObservers();
+export default function() {
 
   return (
-    <ThemeProvider theme={darkTheme}>
-      <div ref={topRef} />
-      <Box sx={{ minHeight: '100vh' }}>
-        <CssBaseline />
-        <Navbar position={position} />
-        <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}>
-          <Splash ref={splashRef} />
-          <Container sx={{ display: 'flex', flexDirection: 'column', gap: 5 }}>
-            <About />
-            <ProjectCards />
-          </Container>
-        </Box>
-      </Box>
-    </ThemeProvider>
+    <h1>allie.</h1>
   );
 }
-
-export default App;
