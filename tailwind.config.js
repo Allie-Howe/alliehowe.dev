99e0dca92a466f1209315bc70f9f3bd7a18780e0
Allie Howe <howeisallie@gmail.com>
2023-06-04 16:49:33 +0100

    total redesign - sans serif font is correct

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
   theme: {
     extend: {},
     fontFamily: {
-      'sans': ['"Open Sans"', 'sans-serif'],
+      'sans': ['Montserrat', 'sans-serif'],
       'serif': ['"Playfair Display"', 'serif'],
     }
   },
