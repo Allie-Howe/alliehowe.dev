45e6e71494f111f394fb8d65a8e88fa4efcacc4b
Allie Howe <howeisallie@gmail.com>
2023-06-04 15:59:56 +0100

    total redesign - shuffle button improvements

--- src/ShuffleButton.tsx
+++ src/ShuffleButton.tsx
@@ -1,37 +1,35 @@
 import ShuffleVariantIcon from 'mdi-react/ShuffleVariantIcon';
 import { Dispatch, SetStateAction, useCallback, useState } from 'react';
-import { getCurrentBreakpoints } from './breakpointUtils';
 import { defaultTransition } from './App';
 import { backgroundMap } from './CustomBackground';
 
 export const pinks = [
-  'rgb(249 168 212)',
-  'rgb(219 39 119)',
-  'rgb(255 0 102)',
-];
+  '#f9a8d5', // Hex version of Tailwind color
+  '#d98db6',
+  ];
 
 interface ShuffleButtonProps {
   setBackgroundIndex: Dispatch<SetStateAction<number>>
 }
 export const ShuffleButton = ({ setBackgroundIndex }: ShuffleButtonProps) => {
-  const [isRndDown, setIsRndDown] = useState(false);
-  const toggleRndDown = useCallback(() => setIsRndDown(p => !p), []);
-  const { lg } = getCurrentBreakpoints();
-  const downColor = lg ? 'rgb(249 168 212)' : 'rgb(219 39 119)';
+  const [isDown, setIsDown] = useState(false);
+  const toggleDown = useCallback(() => setIsDown(p => !p), []);
 
   // TODO: Custom glowing CSS shadow
   return (
-      <div className={`${defaultTransition} h-20 w-20 bg-pink-300 lg:hover:bg-pink-400 rounded-full absolute right-5 top-5 flex items-center justify-center text-black transition-all` }>
-        <ShuffleVariantIcon
-          size='3em'
-          className='transition-all'
-          onClick={() => setBackgroundIndex(i => (i + 1) % backgroundMap.length)}
-          onMouseDown={toggleRndDown}
-          onMouseUp={toggleRndDown}
-          onTouchStart={toggleRndDown}
-          onTouchEnd={toggleRndDown}
-          color={isRndDown ? downColor : undefined}
-        />
-      </div>
+      <button
+        style={{
+          backgroundColor: isDown ? pinks[1] : pinks[0],
+          translate: isDown ? '0 1px' : '0 0'
+        }}
+        className={`${defaultTransition} h-20 w-20 bg-pink-300 rounded-full absolute right-5 top-5 flex items-center justify-center text-black transition-all duration-75` }
+        onClick={() => setBackgroundIndex(i => (i + 1) % backgroundMap.length)}
+        onMouseDown={toggleDown}
+        onMouseUp={toggleDown}
+        onTouchStart={toggleDown}
+        onTouchEnd={toggleDown}
+      >
+        <ShuffleVariantIcon size='3em'/>
+      </button>
   )
 };
