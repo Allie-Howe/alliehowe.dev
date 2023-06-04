bbf67f9be9f64eda3fa90d5c1db89dabdce1bcb2
Allie Howe <howeisallie@gmail.com>
2023-06-04 01:41:31 +0100

    total redesign - game of life

--- src/CustomBackground.tsx
+++ src/CustomBackground.tsx
@@ -1,31 +1,35 @@
 import Sketch from "react-p5";
-import p5Types from "p5"; //Import this for typechecking and intellisense
+import p5 from "p5"; //Import this for typechecking and intellisense
 import { useP5DupeRemover } from './p5DupeRemover';
+import { useGameOfLife } from './p5scripts/gameOfLife';
+
+type DrawFn = () => (p5: p5) => void
+const scriptsMap: DrawFn[]  = [
+  useGameOfLife
+];
 
 export const CustomBackground = () => {
   const setParent = useP5DupeRemover();
+  const drawFns = scriptsMap.map((script) => script());
 
-  const setup = (p5: p5Types, canvasParentRef: Element) => {
+  const setup = (p5: p5, canvasParentRef: Element) => {
     setParent(canvasParentRef);
     p5.createCanvas(p5.windowWidth, p5.windowHeight).parent(canvasParentRef);
     p5.background(0);
+    p5.noStroke();
+    p5.pixelDensity(1);
   };
 
-  const windowResized = (p5: p5Types) => {
+  const windowResized = (p5: p5) => {
     p5.resizeCanvas(p5.windowWidth, p5.windowHeight);
   };
 
-  const draw = (p5: p5Types) => {
-    p5.background(30);
-    p5.stroke(255);
-    p5.strokeWeight(2);
-    for (let i = 0; i < 100; i++) {
-      p5.point(p5.random(0, p5.width), p5.random(0, p5.height));
-    }
+  const draw = (p5: p5) => {
+    drawFns[0](p5);
   };
 
   return (
-    <div className="custom-background absolute">
+    <div style={{filter: 'blur(3px)'}} className="custom-background absolute">
       <Sketch setup={setup} draw={draw} windowResized={windowResized}/>
     </div>
   );
---
+++ src/p5scripts/gameOfLife.ts
@@ -0,0 +1,54 @@
+import p5 from 'p5';
+import { useEffect, useState } from 'react';
+
+const TILE_SIZE = 10;
+
+const cols = {
+  dark: 30,
+  light: 150,
+}
+
+export const useGameOfLife = () => {
+  const gridLength = Math.round(window.innerWidth/TILE_SIZE);
+  const gridHeight = Math.round(window.innerHeight/TILE_SIZE);
+
+  const [gameState, setGameState] = useState(new Array(gridHeight).fill(new Array(gridLength).fill(false)).map(col => col.map(() => Math.random() > 0.9)));
+
+  // TODO: Useeffect that expands/cuts grid to fit screen
+
+  const drawGrid = (p5: p5) => {
+    for (let i = 0; i < gridLength; i++) {
+      for (let j = 0; j < gridHeight; j++) {
+        p5.fill(gameState[j][i] ? cols.light : cols.dark);
+        p5.square(i*TILE_SIZE, j*TILE_SIZE, TILE_SIZE);
+      }
+    }
+  }
+
+  const updateGameState = () => {
+    setGameState(gameState.map((row, y) => row.map((cell, x) => {
+      const neighbours = [
+        gameState[y-1]?.[x-1],
+        gameState[y-1]?.[x],
+        gameState[y-1]?.[x+1],
+        gameState[y]?.[x-1],
+        gameState[y]?.[x+1],
+        gameState[y+1]?.[x-1],
+        gameState[y+1]?.[x],
+        gameState[y+1]?.[x+1],
+      ].filter(Boolean).length;
+      if (cell) {
+        return neighbours === 2 || neighbours === 3;
+      } else {
+        return neighbours === 3;
+      }
+    })));
+  };
+
+  const draw = (p5: p5) => {
+    p5.background(cols.dark);
+    drawGrid(p5);
+    updateGameState();
+  }
+  return draw
+}
