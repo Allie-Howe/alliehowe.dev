5efd4a7d3dcc3fc9ad0ef244171fe41cca66d6f1
Allie Howe <howeisallie@gmail.com>
2023-06-04 17:09:13 +0100

    total redesign - bragging

--- src/components/pages/About.tsx
+++ src/components/pages/About.tsx
@@ -15,7 +15,7 @@ export const About = () => {
         {/* TODO: Add picture */}
         <p className='text-3xl'>about</p>
         <div className='text-white font-sans flex flex-col gap-3'>
-          <p>I'm Allie Howe, a Computer Science BSc (Hons) graduate from the University of Lincoln, currently working for music tech startup <NewTabLink href='http://music.clicknclear.com/'>ClicknClear</NewTabLink> as a Graduate Software Engineer.</p>
+          <p>I'm Allie Howe, a Computer Science BSc (First Class, Hons.) graduate from the University of Lincoln, currently working for music tech startup <NewTabLink href='http://music.clicknclear.com/'>ClicknClear</NewTabLink> as a Graduate Software Engineer.</p>
           <p>Besides this, I enjoy playing around with various technologies - from creative code using p5.js, to learning new frameworks such as Svelte, to improving my knowledge of React and more.</p>
           <p>You can see all of my open source projects, including this site itself, on my <NewTabLink href='https://github.com/allie-howe'>GitHub</NewTabLink>.</p>
         </div>
