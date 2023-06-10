1aaf77c255b2d0d7f042704cc9f0f6577fede7da
Allie Howe <howeisallie@gmail.com>
2023-06-10 21:01:23 +0100

    contact page - links more concise

--- src/components/pages/Contact.tsx
+++ src/components/pages/Contact.tsx
@@ -1,36 +1,48 @@
 import { ContentWrapper } from './ContentWrapper';
-import mail from '../../../public/images/icons/email.svg';
-import linkedIn from '../../../public/images/icons/linkedin.svg';
-import github from '../../../public/images/icons/github.svg';
-import instagram from '../../../public/images/icons/instagram.svg';
+import mailIcon from '../../../public/images/icons/email.svg';
+import liIcon from '../../../public/images/icons/linkedin.svg';
+import ghIcon from '../../../public/images/icons/github.svg';
+import instaIcon from '../../../public/images/icons/instagram.svg';
+import { NewTabLink } from './About';
 
-const EMAIL_ADDRESS = 'hello@alliehowe.dev'
-const LINKEDIN_URL = 'allie-howe'
-const GITHUB_URL = 'allie-howe'
-const INSTAGRAM_URL = 'allie.on.the.web'
+interface Info {
+  url: string
+  username: string
+  logo: string
+}
+const infoArr: Info[] = [
+  {
+    url: 'mailto:',
+    username: 'hello@alliehowe.dev',
+    logo: mailIcon
+  },
+  {
+    url: 'https://www.instagram.com/',
+    username: 'allie.on.the.web',
+    logo: instaIcon
+  },
+  {
+    url: 'https://www.github.com/',
+    username: 'allie-howe',
+    logo: ghIcon
+  },
+  {
+    url: 'https://www.linkedin.com/in/',
+    username: 'allie-howe',
+    logo: liIcon
+  },
+]
 
 export const Contact = () => {
-  // TODO: Make function to improve legibility
-  // TODO: Links should open in new tab
   return (
     <ContentWrapper text='contact'>
-      <div className='text-white font-sans flex flex-col gap-5'>
-        <a href={`mailto:${EMAIL_ADDRESS}`} className='flex justify-center gap-2 items-center'>
-          <img src={mail} className='invert w-10' />
-          <p>{EMAIL_ADDRESS}</p>
-        </a>
-        <a href={`https://www.instagram.com/${INSTAGRAM_URL}`} className='flex justify-center gap-2 items-center'>
-          <img src={instagram} className='invert w-10' />
-          <p>{INSTAGRAM_URL}</p>
-        </a>
-        <a href={`https://www.linkedin.com/in/${LINKEDIN_URL}`} className='flex justify-center gap-2 items-center'>
-          <img src={linkedIn} className='invert w-10' />
-          <p>{LINKEDIN_URL}</p>
-        </a>
-        <a href={`https://www.github.com/${GITHUB_URL}`} className='flex justify-center gap-2 items-center'>
-          <img src={github} className='invert w-10' />
-          <p>{GITHUB_URL}</p>
-        </a>
+      <div className='font-sans flex flex-col gap-5'>
+        {infoArr.map(({logo, url, username}) => (
+          <NewTabLink href={`${url}${username}`} className='flex justify-center gap-2 items-center text-white'>
+            <img src={logo} className='invert w-10' />
+            <p>{username}</p>
+          </NewTabLink>
+        ))}
       </div>
     </ContentWrapper>
   );
