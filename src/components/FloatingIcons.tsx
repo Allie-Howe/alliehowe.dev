180e920da322a8d2c18b27c276ee998233ecabd5
Allie Howe <howeisallie@gmail.com>
2023-10-21 18:42:50 +0100

    structure - Toolbar its own component, no zen on black screen

--- src/components/FloatingIcons.tsx
+++ src/components/FloatingIcons.tsx
@@ -4,16 +4,24 @@ import { ShuffleButton } from './ShuffleButton'
 import { useBackgroundContext } from '../backgrounds/BackgroundContext'
 
 export const FloatingIcons = () => {
-  const {zenMode, setZenMode} = useBackgroundContext();
+  const {zenMode, backgroundIndex} = useBackgroundContext();
 
   return <div className='absolute right-5 top-5'>
     <div className='flex flex-col items-end gap-4'>
       {!zenMode && <ShuffleButton />}
-      <div className='flex flex-col gap-2 text-gray-300 bg-gray-600 bg-opacity-50 rounded-full p-2'>
-        <div onClick={() => setZenMode(p => !p)}>
-          {zenMode ? <FullscreenExitIcon /> :  <FullscreenIcon />}
-        </div>
-      </div>
+      {backgroundIndex !== 0 && <Toolbar />}
     </div>
   </div>
+};
+
+const Toolbar = () => {
+  const {zenMode, setZenMode} = useBackgroundContext();
+
+  return (
+    <div className='flex flex-col gap-2 text-gray-300 bg-gray-600 bg-opacity-50 rounded-full p-2'>
+      <div onClick={() => setZenMode(p => !p)}>
+        {zenMode ? <FullscreenExitIcon /> :  <FullscreenIcon />}
+      </div>
+    </div>
+  )
 };
