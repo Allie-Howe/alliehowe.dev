9d0e44c3f813bba6276eecbf8d2706cc8138ded9
Allie Howe <howeisallie@gmail.com>
2024-11-30 16:10:56 +0100

    blurring background of content

--- src/components/pages/Projects.tsx
+++ src/components/pages/Projects.tsx
@@ -17,36 +17,6 @@ export const Projects = () => {
     </>);
 };
 
-const ProjectOne = ({ project }: {project: Project}) => {
-  return (<div className='growOnHoverWrap flex flex-col justify-between min-h-[300px] rounded-3xl overflow-hidden text-black bg-[#f8f8ffee] hover:bg-[#f8f8ffcc] duration-500'>
-    <div>
-      <div className= 'growOnHoverItem duration-200'>
-        {project.imgUrl.includes('webm')
-          ? (
-            <video disableRemotePlayback autoPlay loop muted>
-                <source src={`images/previews/${project.imgUrl}`} type='video/webm' />
-            </video>
-          )
-          : <img src={`images/previews/${project.imgUrl}`} />
-        }
-      </div>
-      <div className='flex flex-col gap-4 p-2'>
-        <p className='text-2xl text-center'>{project.name}</p>
-        <p className='font-sans'>{project.description}</p>
-      </div>
-    </div>
-    <div>
-      {project.buttons.map((button) => {
-        return (
-          <a className='text-black text-lg bg-pink-200 hover:bg-pink-300 duration-500 p-1 block text-center' href={button.url} target='_blank' rel='noreferrer'>
-            {button.title}
-          </a>
-        )
-      })}
-    </div>
-  </div>)
-};
-
 // TODO: Downscale images to improve performance
 // TODO: Hard-code width of images to reduce layout shift
 const ProjectTwo = ({ project }: {project: Project}) => {
