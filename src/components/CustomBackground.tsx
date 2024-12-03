4361a53b7dc502163230aebc650d0150dc54e550
Allie Howe <howeisallie@gmail.com>
2024-12-03 17:20:33 +0000

    removed weird rotating lines, renamed SquareCircle

--- src/components/CustomBackground.tsx
+++ src/components/CustomBackground.tsx
@@ -1,16 +1,14 @@
 import { useBackgroundContext } from '../backgrounds/BackgroundContext';
 import { GameOfLife } from '../backgrounds/GameOfLife';
-import { PlainBlack, PlainPink } from '../backgrounds/Plains';
-import { Tmp } from '../backgrounds/Tmp';
-import { Tmp2 } from '../backgrounds/Tmp2';
+import { PlainBlack } from '../backgrounds/Plains';
+import { SquareCircle } from '../backgrounds/SquareCircle';
 import { TwinCircles } from '../backgrounds/TwinCircles';
 
 export const backgroundMap = [
   PlainBlack,
   GameOfLife,
   TwinCircles,
-  Tmp,
-  Tmp2,
+  SquareCircle,
   // If we're gonna use this, we need the text to change dynamically - maybe use context to store index & other data?
   // PastelRainbow,
 ]
