d760c2bdbb12590b3bb50b3e94d0b909dff8eaec
Allie Howe <howeisallie@gmail.com>
2024-12-03 18:21:22 +0000

    added drop shadow to splash text

--- src/components/SplashScreen.tsx
+++ src/components/SplashScreen.tsx
@@ -1,1 +1,1 @@
-export const SplashScreen = () => <p className='text-9xl'>allie.</p>;
+export const SplashScreen = () => <p className='text-9xl' style={{filter: 'drop-shadow(0 0 0.8rem black)'}}>allie.</p>;
